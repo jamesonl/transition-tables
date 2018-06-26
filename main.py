06dd9047d1171e939e60b89af63434fcd3b3514b
Jameson Lee <jamesonlee@jamesons-mbp.attlocal.net>
2018-06-25 21:58:55 -0700

    created working transition tables. This is flexible enough to calculate the conditional probability of any ngram-combination of words

--- main.py
+++ main.py
@@ -3,23 +3,53 @@ from nltk import sent_tokenize, word_tokenize, ngrams
 import nltk
 import os
 import string
+from collections import defaultdict
 
-def word_grams(words, min=1, max=4):
-    s = []
-    for n in range(min, max):
-        for ngram in ngrams(words, n):
-            s.append(' '.join(str(i) for i in ngram))
-    return s
 
 def main():
-    comments = pd.read_csv('test_comments_brands.csv', names = ['polarity', 'id', 'date', 'query', 'user', 'tweet']).head(5)
-    # print comments
-    # comment_list = [ngrams(gram, 2) for gram in comments['tweet'].tolist()]
+    comments = pd.read_csv('test_comments_brands.csv', names = ['polarity', 'id', 'date', 'query', 'user', 'tweet']).head(500)
     comment_list = [sent_tokenize(sent) for sent in comments['tweet'].tolist()]
-    # sea_text = [item for sublist in comment_list for item in sublist]
-    for c in comment_list:
-        ngram_statement = ' '.join(str(i) for i in ngrams([iter for iter in word_tokenize(str(c)) if iter not in string.punctuation], 3))
-        print ngram_statement
+    entropylist = defaultdict(list)
+
+    removals = string.punctuation + '``'
+    for com in comment_list:
+        ngram_statement = [str(i) for i in ngrams([iter for iter in word_tokenize(str(com)) if iter not in removals], 1)]
+        counter = 0
+        recent_list = []
+        for gram in ngram_statement:
+            gram_clean = gram[2:len(gram)-3]
+            if counter == 0:
+                entropylist['[start]'].append([gram_clean.lower()])
+                recent_list.append([gram_clean.lower()])
+            else:
+                entropylist[len(recent_list)-1].append(str(gram_clean.lower()))
+                recent_list.append([gram_clean.lower()])
+            counter += 1
+
+    # for x in entropylist:
+    #     print x, len(entropylist[x])
+
+    # create the conditional probability list to calculate odds based on the word
+    all_entropy = {}
+    for key in entropylist:
+        count_vals = {}
+        words_connect = len(entropylist[key])
+        # print words_connect
+        # print key
+        for val in entropylist[key]:
+            if str(val) in count_vals:
+                count_vals[str(val)] += 1
+            else:
+                count_vals[str(val)] = 1
+
+        cond_prob_val = {}
+        for count_info in count_vals:
+            cond_prob_val[count_info] = float(count_vals[count_info]) / float(words_connect)
+        all_entropy[key] = [cond_prob_val]
+
+    for x in all_entropy['[start]']:
+        for y in x:
+            print y, x[y]
         print '\n'
 
 main()
